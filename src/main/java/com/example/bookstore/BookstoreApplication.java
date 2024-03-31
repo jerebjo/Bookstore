baafa30804755d4f8c431a2b6f20f4462bf21853
jerebjo <148748911+jerebjo@users.noreply.github.com>
2024-03-31 13:08:52 +0300

    päivitys

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package com.example.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,33 +11,43 @@ import com.example.bookstore.model.Book;
 import com.example.bookstore.model.BookRepository;
 import com.example.bookstore.model.Category;
 import com.example.bookstore.model.CategoryRepository;
+import com.example.bookstore.model.User;
+import com.example.bookstore.model.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository bookRepository, CategoryRepository categoryRepository) {
+	public CommandLineRunner demo(BookRepository bookRepository, CategoryRepository categoryRepository,
+			UserRepository userRepository) {
 		return (args) -> {
+			Category fantasy = categoryRepository.save(new Category("Fantasy"));
+			Category comic = categoryRepository.save(new Category("Comic"));
+			Category thriller = categoryRepository.save(new Category("Thriller"));
+			Category romance = categoryRepository.save(new Category("Romance"));
 
-			// Luodaan muutamia kategorioita ja tallennetaan ne tietokantaan
-			Category comic = new Category("Comic");
-			Category fantasy = new Category("Fantasy");
-
-			categoryRepository.save(comic);
-			categoryRepository.save(fantasy);
-
-			// Luodaan muutamia kirjoja ja asetetaan niille kategoriat
-			Book book1 = new Book("Kirja 1", "Jukka", "2002", "33498290", "30", comic);
+			Book book1 = new Book("Kirja 1", "Jukka", "2002", "33498290", "30", fantasy);
 			Book book2 = new Book("Kirja 2", "Jari", "2006", "33499402", "20", comic);
-
 			bookRepository.save(book1);
 			bookRepository.save(book2);
 
-			System.out.println("Kategoriat ja kirjat tallennettu tietokantaan.");
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER",
+					"user.user@gmail.com");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN",
+					"admin.admin@gmail.com");
+			userRepository.save(user1);
+			userRepository.save(user2);
+
+			log.info("fetch all books");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
 		};
 	}
 }
